a5192af0a8743b6f18f12c49db6b353bd09067fb
Farou <86537710+Fakhri-Amin@users.noreply.github.com>
2025-03-20 23:19:28 +0900

    Added audio reference SO

---
+++ Potion Promise/Assets/Codes/Scripts/ScriptableObject/GameSoundSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameSoundSO", menuName = "Eggtato/Game Sound")]
+public class GameSoundSO : ScriptableObject
+{
+    public AudioClip ClickSound;
+    public AudioClip TypeSound;
+}
--- Potion Promise/Assets/Codes/Scripts/Utility/AudioManager.cs
+++ Potion Promise/Assets/Codes/Scripts/Utility/AudioManager.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using MoreMountains.Feedbacks;
 using Eggtato.Utility;
+using UnityEngine.Rendering;
+using MoreMountains.Tools;
 
 public class AudioManager : PersistentSingleton<AudioManager>
 {
-    [SerializeField] private MMFeedbacks uiClickFeedbacks;
-    [SerializeField] private MMFeedbacks typeSoundFeedbacks;
+    [SerializeField] private GameSoundSO gameSoundSO;
 
     public void PlayClickSound()
     {
-        uiClickFeedbacks.PlayFeedbacks();
+        PlaySFX(gameSoundSO.ClickSound);
     }
 
     public void PlayTypeSound()
     {
-        typeSoundFeedbacks.PlayFeedbacks();
+        PlaySFX(gameSoundSO.TypeSound);
+    }
+
+    private void PlaySFX(AudioClip audio)
+    {
+        PlaySound(audio, MMSoundManager.MMSoundManagerTracks.Sfx);
+    }
+
+    private void PlayMusic(AudioClip audio)
+    {
+        PlaySound(audio, MMSoundManager.MMSoundManagerTracks.Music);
+    }
+
+    private void PlaySound(AudioClip audio, MMSoundManager.MMSoundManagerTracks track)
+    {
+        MMSoundManagerSoundPlayEvent.Trigger(audio, track, transform.position);
     }
 }
